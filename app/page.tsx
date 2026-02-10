78e1cfa38a7e7808dc5c6acf96b2582d60d19696
satyaritesh <riteshnaidu14@gmail.com>
2026-02-10 20:21:52 +0530

    twenty-three

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import Navbar from '@/components/main/Navbar';
 import FlareCursor from '@/components/main/Cursor';
 
 const StarsCanvas = dynamic(() => import('@/components/main/StarCanvas'), { ssr: false });
-const Gallery = dynamic(() => import('@/components/main/Gallery'), { ssr: false });
-const About = dynamic(() => import('@/components/main/About'), { ssr: false });
-const PastPerformers = dynamic(() => import('@/components/main/PastPerformers'), { ssr: false });
-const Proshows = dynamic(() => import('@/components/main/Proshows'), { ssr: false });
-const Footer = dynamic(() => import('@/components/main/Footer'), { ssr: false });
-const ZoomParallax = dynamic(() => import('@/components/main/ZoomParallax'), { ssr: false });
-const SponsorSection = dynamic(() => import('@/components/main/Sponser').then(mod => mod.SponsorSection), { ssr: false });
+const Gallery = dynamic(() => import('@/components/main/Gallery'));
+const About = dynamic(() => import('@/components/main/About'));
+const PastPerformers = dynamic(() => import('@/components/main/PastPerformers'));
+const Proshows = dynamic(() => import('@/components/main/Proshows'));
+const Footer = dynamic(() => import('@/components/main/Footer'));
+const ZoomParallax = dynamic(() => import('@/components/main/ZoomParallax'));
+const SponsorSection = dynamic(() => import('@/components/main/Sponser').then(mod => mod.SponsorSection));
 
 const Home: React.FC = () => {
   return (
