f37f728abe6708fb4956c9cd8368af1648bd1d4a
satyaritesh <riteshnaidu14@gmail.com>
2026-02-12 10:21:13 +0530

    Texpo Link Fixed

--- components/main/TechnicalCompetition.tsx
+++ components/main/TechnicalCompetition.tsx
@@ -22,12 +22,12 @@ const Technical: React.FC<Types> = ({ types }) => {
       </h1>
       <div className='flex flex-col items-center justify-center md:flex-row md:justify-center'>
         <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3'>
-      
+
           <div>
             < Card
               imageSrc="/images/TECHNICAL/BGMI Master Series.png"
               title="BGMI Master Series"
-              initialDescription="BGFS is a Battlegrounds Mobile India tournament conducted exclusively for first-year students. This event provides a platform for freshers to demonstrate their gaming skills, strategic thinking, and team coordination in a competitive environment.Participants will compete for attractive prizes, with the winning team securing a direct entry into the official roster selection process for the current academic year." 
+              initialDescription="BGFS is a Battlegrounds Mobile India tournament conducted exclusively for first-year students. This event provides a platform for freshers to demonstrate their gaming skills, strategic thinking, and team coordination in a competitive environment.Participants will compete for attractive prizes, with the winning team securing a direct entry into the official roster selection process for the current academic year."
               linkTo={'https://www.canva.com/design/DAHA53V8WqU/3GH6T1BgZ-fD1Ty7cd1IOA/edit?utm_content=DAHA53V8WqU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 9315268810"
@@ -41,7 +41,7 @@ const Technical: React.FC<Types> = ({ types }) => {
             < Card
               imageSrc="/images/TECHNICAL/VR Tennis.png"
               title="VR Tennis"
-              initialDescription="Sponsored Event Under University Esports Championship" 
+              initialDescription="Sponsored Event Under University Esports Championship"
               linkTo={'https://www.canva.com/design/DAHA5_oKB3A/HZccwyw1LZk_WX6xRJ2W4w/edit?utm_content=DAHA5_oKB3A&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 9265025088"
@@ -55,7 +55,7 @@ const Technical: React.FC<Types> = ({ types }) => {
             < Card
               imageSrc="/images/TECHNICAL/Valorant Vanguard Series.png"
               title="Valorant Vanguard Series"
-              initialDescription="Valorant Freshers Series is a college esports tournament conducted exclusively for BTech first-year students. This event provides a platform for freshers to showcase their gaming skills, teamwork, and strategic thinking in an intense competitive environment. Participants will compete for attractive prizes, with the winning team earning recognition and a chance to represent the college in higher-level Valorant competitions. Step into the arena and prove your mettle in this thrilling campus showdown." 
+              initialDescription="Valorant Freshers Series is a college esports tournament conducted exclusively for BTech first-year students. This event provides a platform for freshers to showcase their gaming skills, teamwork, and strategic thinking in an intense competitive environment. Participants will compete for attractive prizes, with the winning team earning recognition and a chance to represent the college in higher-level Valorant competitions. Step into the arena and prove your mettle in this thrilling campus showdown."
               linkTo={'https://www.canva.com/design/DAHA5-TKhh8/-4Statke5jDTB8IaLTdk2Q/edit?utm_content=DAHA5-TKhh8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 7702068993"
@@ -69,7 +69,7 @@ const Technical: React.FC<Types> = ({ types }) => {
             < Card
               imageSrc="/images/TECHNICAL/Real Cricket.png"
               title="Real Cricket"
-              initialDescription="Real Cricket Freshers Series is a competitive mobile cricket tournament organized exclusively for BTech first-year students. The event serves as a platform for freshers to demonstrate their cricketing skills, tactical play, and team coordination in a spirited environment. Participants compete for attractive prizes, with the winning squad getting a chance to enter the official campus esports selection for the academic year. Step onto the digital pitch and showcase your talent in this exciting cricket showdown." 
+              initialDescription="Real Cricket Freshers Series is a competitive mobile cricket tournament organized exclusively for BTech first-year students. The event serves as a platform for freshers to demonstrate their cricketing skills, tactical play, and team coordination in a spirited environment. Participants compete for attractive prizes, with the winning squad getting a chance to enter the official campus esports selection for the academic year. Step onto the digital pitch and showcase your talent in this exciting cricket showdown."
               linkTo={'https://www.canva.com/design/DAHA54DRh8Y/UhKz3oe6c4sdT4Au713ZFQ/edit?utm_content=DAHA54DRh8Y&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 9537530501"
@@ -83,7 +83,7 @@ const Technical: React.FC<Types> = ({ types }) => {
             < Card
               imageSrc="/images/TECHNICAL/Tech - Drone Soccer.png"
               title="Drone Soccer"
-              initialDescription="International FPV Drone Race is a high-speed competitive event conducted at global racing standards, where skilled pilots race FPV drones through a professionally designed track. Following MultiGP-style formats, the event showcases precision flying, advanced drone technology, and real-time competition, making it one of the most exciting technical highlights of the fest." 
+              initialDescription="International FPV Drone Race is a high-speed competitive event conducted at global racing standards, where skilled pilots race FPV drones through a professionally designed track. Following MultiGP-style formats, the event showcases precision flying, advanced drone technology, and real-time competition, making it one of the most exciting technical highlights of the fest."
               linkTo={'https://www.canva.com/design/DAHA5-YI12I/onaN2nUCCDmGApsvKaLA_g/edit?utm_content=DAHA5-YI12I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 9893986159"
@@ -98,7 +98,7 @@ const Technical: React.FC<Types> = ({ types }) => {
               imageSrc="/images/TECHNICAL/Line Following Bot.png"
               title="Line Following Bot"
               initialDescription="The Line Following Bot Race is a speed-based autonomous robotics challenge where participants build a robot capable of tracking and following a line-based path. The bot must complete the predefined track in the least possible time using sensors and programmed logic.
-The course may include curves, junctions, and varying speeds to test precision and stability." 
+The course may include curves, junctions, and varying speeds to test precision and stability."
               linkTo={'https://www.canva.com/design/DAHA58V5Anw/0TWUdFaufrrhOIFRuRa2Jg/edit?utm_content=DAHA58V5Anw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 6009923073"
@@ -115,7 +115,7 @@ The course may include curves, junctions, and varying speeds to test precision a
               initialDescription="The Obstacle Course Challenge is an engineering-based robotics competition where teams design and operate a wired or wireless robot to complete a dynamic obstacle track.
 The course includes uneven terrains, ramps, turns, and physical objects that test handling, stability,and navigation skills.
 Participants must complete the entire track in the shortest possible time, while avoiding penalties.
-The track design will be revealed only on the day of competition." 
+The track design will be revealed only on the day of competition."
               linkTo={'https://www.canva.com/design/DAHA536yCWw/MKYrlWyKBY24btLbdy8k9A/edit?utm_content=DAHA536yCWw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 7702068993"
@@ -129,7 +129,7 @@ The track design will be revealed only on the day of competition."
             < Card
               imageSrc="/images/TECHNICAL/Texpo.png"
               title="Code Chase Competition"
-              initialDescription="TEXPO 2026 is the flagship technology expo organized by the HackShastra SRM–AP Chapter and marks the official launch of the HackShastra SRM–AP Student Chapter. The event is designed to bridge student innovation with industry application through a structured online evaluation round followed by a large-scale offline project and industry expo." 
+              initialDescription="TEXPO 2026 is the flagship technology expo organized by the HackShastra SRM–AP Chapter and marks the official launch of the HackShastra SRM–AP Student Chapter. The event is designed to bridge student innovation with industry application through a structured online evaluation round followed by a large-scale offline project and industry expo."
               linkTo={'https://www.canva.com/design/DAHA56O8aPc/ij-6ntIyP3I0lZo2rdfUXg/edit?utm_content=DAHA56O8aPc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'}
             />
             <EventDetails
@@ -137,7 +137,7 @@ The track design will be revealed only on the day of competition."
               date="25-28 February 2026"
               time="30,000 /-"
               href="https://forms.gle/3pVoNyFTW6Rv6tGx6"
-              href2='https://unstop.com/p/texpo-26-infinitus-2026-srm-university-srmap-andhra-pradesh-1638541'
+              href2='https://unstop.com/competitions/texpo-2026-student-innovation-and-industry-technology-expo-srm-university-srmap-andhra-pradesh-1638340'
             />
           </div>
           <div>
@@ -146,7 +146,7 @@ The track design will be revealed only on the day of competition."
               title="Your Event Title"
               initialDescription="This seminar aims to deliver a high-energy, interactive, and concept-driven experience that showcases the cutting-edge technologies being developed inside Google — beyond what most students see or use every day.
 Through live demonstrations, thought-provoking discussions, and fun audience games, the session introduces real-world applications of Artificial Intelligence (AI), Augmented Reality (AR), and emerging Google projects — all without requiring laptops or coding participation.
-The session is designed specifically for engineering students who already understand core technologies and are curious about what’s next in innovation." 
+The session is designed specifically for engineering students who already understand core technologies and are curious about what’s next in innovation."
               linkTo={'https://www.canva.com/design/DAHA57Oms_U/0y_ZyKElX3bGitEUROwNwQ/edit?utm_content=DAHA57Oms_U&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 6009923073"
@@ -161,7 +161,7 @@ The session is designed specifically for engineering students who already unders
               imageSrc="/images/TECHNICAL/Robotics Reimagined.png"
               title="Your Event Title"
               initialDescription="An insightful session conducted by an industry expert, aimed at introducing students to the fundamentals of ROS and its applications. This seminar will provide participants with an in-depth understanding of control systems and real-world applications of ROS in aerial robotics.
-The session will bridge the gap between academic learning and industry practices, offering students exposure to the latest developments in drone automation and software integration" 
+The session will bridge the gap between academic learning and industry practices, offering students exposure to the latest developments in drone automation and software integration"
               linkTo={'https://www.canva.com/design/DAHA5_LMY-I/FFG9MC4ESWCBrIKGodn2Ow/edit?utm_content=DAHA5_LMY-I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 6009923073"
@@ -190,7 +190,7 @@ The session will bridge the gap between academic learning and industry practices
               imageSrc="/images/TECHNICAL/PCB Workshop.png"
               title="PCB Workshop"
               initialDescription="This beginner-friendly, hands-on PCB Workshop introduces students to Printed Circuit Board (PCB) design from scratch using simulation software. Participants will learn PCB basics, electronic symbols, footprints, schematic creation, PCB layout routing and exporting Gerber files.
-The workshop aims to equip learners with industry-relevant hardware design skills. By the end of the workshop, participants will complete their own PCB design inside the software, helping them gain confidence in electronics hardware development." 
+The workshop aims to equip learners with industry-relevant hardware design skills. By the end of the workshop, participants will complete their own PCB design inside the software, helping them gain confidence in electronics hardware development."
               linkTo={'https://www.canva.com/design/DAHA534TS6w/NXsPwGxuRnnLEOohCz6rig/edit?utm_content=DAHA534TS6w&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 7061828823"
@@ -204,7 +204,7 @@ The workshop aims to equip learners with industry-relevant hardware design skill
             < Card
               imageSrc="/images/TECHNICAL/Tech Sprint Robotics Reimagined.png"
               title="Your Event Title"
-              initialDescription="Workshop session for related to robotics for drones which will cover the robotics frame work using robotics operating system 2 along with tutorials on Gazebo and Rviz simulations" 
+              initialDescription="Workshop session for related to robotics for drones which will cover the robotics frame work using robotics operating system 2 along with tutorials on Gazebo and Rviz simulations"
               linkTo={'https://www.canva.com/design/DAHA56bugi0/SGz_DC8yrF6pN6D9u00U0w/edit?utm_content=DAHA56bugi0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'} />
             <EventDetails
               venue="+91 7061828823"
